f2e0cab21783b666997736c69c6d25367d6fb7a9
Gabriela Dyworová <gabriela.dyworova@gmail.com>
2022-11-08 13:06:08 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Zadej nějaké číslo, ze kterého spočítám odmocninu:");
+int a = int.Parse(Console.ReadLine());
+if (a > 0)
+{
+    Console.WriteLine("Zadal jsi číslo větší než 0, to znamená, že ho mohu odmocnit!");
+    double o = Math.Sqrt(a);
+    Console.WriteLine("Odmocnina z čísla " + a + " je " + o);
+}
+else
+    Console.WriteLine("Odmocnina ze záporného čísla neexistuje!");
+Console.WriteLine("Děkuji za zadání");
+Console.ReadKey();
